b828071c6864845bd52b1a3b869d41e14182188c
Olga Listerenko <57359954+Milorann@users.noreply.github.com>
2023-03-11 14:57:32 +0300

    Update task.c

--- 4/task.c
+++ 4/task.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 const int buf_size = 5000;
 
@@ -13,86 +15,112 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    int fd[2], result;
-    size_t size;
+    int first_to_second_fd[2], result1;
+    ssize_t size1;
     char str_buf[buf_size];
 
-    if (pipe(fd) < 0)
+    if (pipe(first_to_second_fd) < 0)
     {
-        printf("Can\'t open pipe\n");
+        printf("Can\'t open the first pipe\n");
         exit(-1);
     }
 
-    result = fork();
-    if (result < 0)
+    result1 = fork();
+    if (result1 < 0)
     {
         printf("Can\'t fork child\n");
         exit(-1);
     }
-    else if (result > 0)
+    else if (result1 > 0)
     { // first process
-        if (close(fd[0]) < 0)
+        if (close(first_to_second_fd[0]) < 0)
         {
-            printf("first: Can\'t close reading side of pipe\n");
+            printf("first: Can\'t close reading side of the first pipe\n");
             exit(-1);
         }
 
-        // read from file
-        FILE *myfile;
-        myfile = fopen(argv[1], "r");
-        if (myfile == NULL)
-        {
-            printf("Couldn't open the input file\n");
-            exit(-1);
-        }
+        int rfd;
         char text[buf_size];
-        int i = 0;
-        while ((text[i] = fgetc(myfile)) != EOF || i < buf_size)
+        ssize_t read_bytes;
+        if ((rfd = open(argv[1], O_RDONLY)) < 0)
         {
-            i++;
-        }
-        text[i] = '\0';
-        text[buf_size - 1] = '\0';
-        fclose(myfile);
-
-        size = write(fd[1], text, buf_size);
-        if (size != buf_size)
-        {
-            printf("first: Can\'t write the whole string to pipe\n");
+            printf("first: Can\'t open the input file\n");
             exit(-1);
         }
-        if (close(fd[1]) < 0)
+        read_bytes = read(rfd, text, buf_size);
+        if (read_bytes == -1)
         {
-            printf("first: Can\'t close writing side of pipe\n");
+            printf("first: Can\'t read the input file\n");
+            exit(-1);
+        }
+        text[read_bytes] = '\0';
+        // printf("%s\n", text);
+        if (close(rfd) < 0)
+        {
+            printf("first: Can\'t close the input file\n");
+        }
+
+        size1 = write(first_to_second_fd[1], text, read_bytes);
+        if (size1 != read_bytes)
+        {
+            printf("first: Can\'t write the whole string to the first pipe\n");
+            exit(-1);
+        }
+        if (close(first_to_second_fd[1]) < 0)
+        {
+            printf("first: Can\'t close writing side of the first pipe\n");
             exit(-1);
         }
         printf("first: exit\n");
+        // printf("first: text:\n%s\n", text);
     }
     else
     { // second process
-        result = fork();
-        if (result < 0)
+        int second_to_third_fd[2], result2;
+        ssize_t size2;
+
+        if (pipe(second_to_third_fd) < 0)
+        {
+            printf("Can\'t open the second pipe\n");
+            exit(-1);
+        }
+
+        result2 = fork();
+        if (result2 < 0)
         {
             printf("second: Can\'t fork child\n");
             exit(-1);
         }
-        else if (result > 0)
+        else if (result2 > 0)
         {
-            size = read(fd[0], str_buf, buf_size);
-            if (size < 0)
+            if (close(first_to_second_fd[1]) < 0)
             {
-                printf("second: Can\'t read string from pipe\n");
+                printf("second: Can\'t close writing side of the first pipe\n");
                 exit(-1);
             }
 
-            if (close(fd[0]) < 0)
+            size1 = read(first_to_second_fd[0], str_buf, buf_size);
+            if (size1 < 0)
             {
-                printf("second: Can\'t close reading side of pipe\n");
+                printf("second: Can\'t read string from the first pipe\n");
                 exit(-1);
             }
 
+            if (close(first_to_second_fd[0]) < 0)
+            {
+                printf("second: Can\'t close reading side of the first pipe\n");
+                exit(-1);
+            }
+
+            if (close(second_to_third_fd[0]) < 0)
+            {
+                printf("second: Can\'t close reading side of the second pipe\n");
+                exit(-1);
+            }
+
+            // printf("second: str_buf:\n%s\n", str_buf);
             int numbers[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            for (int i = 0; i < buf_size; i++)
+            for (int i = 0; i < size1; i++)
             {
                 if (str_buf[i] >= 48 && str_buf[i] <= 57)
                 {
@@ -100,15 +128,15 @@ int main(int argc, char *argv[])
                 }
             }
 
-            size = write(fd[1], numbers, sizeof(int) * 10);
-            if (size != sizeof(int) * 10)
+            size2 = write(second_to_third_fd[1], numbers, sizeof(int) * 10);
+            if (size2 != sizeof(int) * 10)
             {
-                printf("second: Can\'t write all numbers to pipe\n");
+                printf("second: Can\'t write all numbers to the second pipe\n");
                 exit(-1);
             }
-            if (close(fd[1]) < 0)
+            if (close(second_to_third_fd[1]) < 0)
             {
-                printf("second: Can\'t close writing side of pipe\n");
+                printf("second: Can\'t close writing side of the second pipe\n");
                 exit(-1);
             }
 
@@ -116,38 +144,50 @@ int main(int argc, char *argv[])
         }
         else
         { // third process
-            if (close(fd[1]) < 0)
+            if (close(second_to_third_fd[1]) < 0)
             {
-                printf("third: Can\'t close writing side of pipe\n");
+                printf("third: Can\'t close writing side of the second pipe\n");
                 exit(-1);
             }
             int numbers[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-            size = read(fd[0], numbers, sizeof(int) * 10);
-            if (size < 0)
+            size2 = read(second_to_third_fd[0], numbers, sizeof(int) * 10);
+            if (size2 != sizeof(int) * 10)
             {
-                printf("third: Can\'t read string from pipe\n");
+                printf("third: Can\'t read the whole array from the second pipe\n");
                 exit(-1);
             }
 
-            if (close(fd[0]) < 0)
+            if (close(second_to_third_fd[0]) < 0)
             {
-                printf("third: Can\'t close reading side of pipe\n");
+                printf("third: Can\'t close reading side of the second pipe\n");
                 exit(-1);
             }
 
-            FILE *myfile;
-            myfile = fopen(argv[2], "w");
-            if (myfile == NULL)
+            int wfd;
+            int str_sz;
+            ssize_t size;
+            char string[100];
+            if ((wfd = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0)
             {
-                printf("Couldn't open the output file\n");
+                printf("third: Can\'t open the output file\n");
                 exit(-1);
             }
             for (int i = 0; i < 10; i++)
             {
-                fprintf(myfile, "%d: %d\n", i, numbers[i]);
+                str_sz = sprintf(string, "%d: %d\n", i, numbers[i]);
+                size = write(wfd, string, str_sz);
+                if (size != str_sz)
+                {
+                    printf("third: Can\'t write all numbers\n");
+                    exit(-1);
+                }
             }
-            fclose(myfile);
+            if (close(wfd) < 0)
+            {
+                printf("third: Can\'t close the output file\n");
+                exit(-1);
+            }
 
             printf("third: exit, the number of digits is written to the output file\n");
         }
